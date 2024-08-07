a63f80f60fce0365f6b898571c2a698e39ca66b9
Radi Rusadi <radi.rusadi.s@gmail.com>
2024-08-07 21:30:18 +0700

    feat: configure the server to serve the static source build

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { serveStatic } from "hono/bun";
 
 import { expensesRoute } from "./routes/expenses";
 
@@ -7,8 +8,9 @@ const app = new Hono();
 
 app.use(logger());
 
-app.get("/", (c) => c.text("Hono!"));
-
 app.route("/api/expenses", expensesRoute);
+
+app.get("*", serveStatic({ root: "./frontend/dist" }));
+app.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
 
 export default app;
